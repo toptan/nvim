e8e5448821d01333c11c1f4ce06166481ef98983
Toplica Tanasković <toplicius@gmail.com>
2025-10-18 21:46:16 +0200

    Added Lazy plugin manager.

---
+++ lua/config/globals.lua
@@ -0,0 +1,14 @@
+-- Setting up the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- We do have nerd font
+vim.g.have_nerd_font = true
+
+-- Disable provider warnings.
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
+-- Setting providers
+vim.g.python3_host_prog = "python3"
