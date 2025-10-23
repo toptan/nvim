b6e568514d87f93889b20c878453a2977ea66d10
Toplica Tanasković <toplicius@gmail.com>
2025-10-24 01:35:00 +0200

    Adding key bindint '-' to open MiniFiles in the directory of the current buffer.

--- lua/config/mini/files.lua
+++ lua/config/mini/files.lua
@@ -1,3 +1,6 @@
 require("mini.files").setup()
 
 vim.keymap.set("n", "<leader>fe", MiniFiles.open, { desc = "Via [E]xplorer" })
+vim.keymap.set("n", "-", function()
+  MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+end, { desc = "Current buffer directory" })
